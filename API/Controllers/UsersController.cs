985fe6727ed2740eaf67638f4860ceee883e8bfe
Vỹ Phạm <vy.phamdonggia@hcmut.edu.vn>
2022-04-28 20:33:14 +0700

    Vy chinh sua lan 1

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -19,7 +19,7 @@ namespace API.Controllers
             _context = context;
         }
 
-        // api/users
+        // api/users all users
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
