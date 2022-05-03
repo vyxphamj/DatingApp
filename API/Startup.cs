4f1a9180129cc9bedbef5015f16a6e7d98dfa4ec
Vỹ Phạm <vy.phamdonggia@hcmut.edu.vn>
2022-05-03 22:13:20 +0700

    Get API Successfully

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +41,11 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
